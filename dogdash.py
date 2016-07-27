cdc1df5135c1ecf5005482452b37b4a3584eb8bd
octavien7 <brhspam@gmail.com>
2016-07-26 22:03:05 -0400

    removing tokens

--- dogdash.py
+++ dogdash.py
@@ -2,8 +2,8 @@ from scapy.all import *
 from twilio.rest import TwilioRestClient
 import time
 
-account_sid = "AC8a2dfaba720a292bbdc4fb953e8434cd" # Your Account SID from www.twilio.com/console
-auth_token  = "8c3bfb8b0b556781833cf744a69f4489"  # Your Auth Token from www.twilio.com/console
+account_sid = "accountoken" # Your Account SID from www.twilio.com/console
+auth_token  = "authtoken"  # Your Auth Token from www.twilio.com/console
 client = TwilioRestClient(account_sid, auth_token)
 
 def arp_display(pkt):
@@ -14,7 +14,7 @@ def arp_display(pkt):
     time_var = 'Dogs were fed at ' + time.strftime("%a %b %d %Y %H:%M:%S %p")
 
     message = client.messages.create(body=time_var,
-          to="+17036566048",    # Replace with your phone number
+          to="+1234567890",    # Replace with your phone number
           from_="+17036596930") # Replace with your Twilio number
 
         #print(message.sid)
